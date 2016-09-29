a753414a2f10190b7aab3240e46e6aae2496c8a5
PyromanicToaster <hor0006@hotmail.com>
2016-09-29 22:07:43 +1000

    Create basic GET endpoint with dummy data

---
+++ BlogBackend/Controllers/PostController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BlogBackend.Models;
+
+namespace BlogBackend.Controllers
+{
+    public class PostController : ApiController
+    {
+        IList<Post> posts = new List<Post> {
+                new Post {Author = "Andrew Hore", Content = "Hello World!", Created = DateTime.UtcNow - TimeSpan.FromDays(7), Id = 1},
+                new Post {Author = "Andrew Hore", Content = "Hello World the Second?", Created = DateTime.UtcNow, Id = 2}
+            };
+
+        public IEnumerable<Post> GetAllPosts()
+        {
+            return posts;
+        }
+
+        public Post GetPost(int id)
+        {
+            return posts.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
---
+++ BlogBackend/Models/Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlogBackend.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public DateTime Created { get; set; }
+        public string Content { get; set; }
+    }
+}
